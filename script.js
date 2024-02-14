0bbadad125891273ca596826f6876fa53b66a3f7
J-Groesbeck <jacob.groesbeck@icloud.com>
2024-02-14 13:48:57 -0500

    A bit of everything

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-let name = ''
+let userName = ''
 
 function inputName() {
-    let name = prompt('Enter your name:')
-    // Hide the input name button and show the game
-    // Display the name in all appropriate places
+    userName = prompt('Enter your name:')
+    document.getElementById('input-name').style.display = "none" // Hide the input name button
+    document.getElementById('game').style.display = "inline" // Show the game
+    document.querySelectorAll('.name').forEach(element => element.innerText = userName) // Display the name in all appropriate places
 }
 
 let playerScore = 0
@@ -13,66 +14,74 @@ function oneRound(playerChoice) {
     let computerArray = ['rock', 'paper', 'scissors']
     let randomNumber = Math.floor(Math.random() * computerArray.length)
     let computerChoice = computerArray[randomNumber]
-    
-    // Display playerChoice
-    // Display computerChoice
 
-    if(computerChoice === playerChoice) {
-        console.log('draw') // Display "Its a draw!"
-        // Do not add score
-    }
+    // Display choices
+    document.getElementById('player-choice').innerText = playerChoice
+    document.getElementById('player-choice').innerText = computerChoice
 
-    if(playerChoice === 'rock') {
-        if(computerChoice === 'paper') {
-            // Display "Computer wins the round! You suck!"
-            computerScore = computerScore + 1 // Add to computer score
+    // Variables for declaring results
+    let winMessage = `${userName} wins the round! You don't suck!`
+    let loseMessage = `Computer wins the round! You suck!`
+    let roundEndMessageDiv = document.getElementById('round-end-message')
+
+    if (computerChoice === playerChoice) {
+        roundEndMessageDiv.innerText = "It's a draw!" //Display "It's a draw!"
+    } else {
+        (playerChoice === 'rock') {
+            if (computerChoice === 'paper') {
+                roundEndMessageDiv.innerText = loseMessage // Display "Computer wins the round! You suck!"
+                computerScore = computerScore + 1 // Add to computer score
+            }
+            if (computerChoice === 'scissors') {
+                roundEndMessageDiv.innerText = winMessage // Display "Player wins the round! You don't suck!"
+                playerScore = playerScore + 1 //Add to player score
+            }
         }
-        if(computerChoice === 'scissors') {
-            // Display "Player wins the round! You don't suck!"
-            playerScore = playerScore + 1 //Add to player score
+        if (playerChoice === 'paper') {
+            if (computerChoice === 'scissors') {
+                roundEndMessageDiv.innerText = loseMessage // Display "Computer wins the round! You suck!"
+                computerScore = computerScore + 1 // Add to computer score
+            }
+            if (computerChoice === 'rock') {
+                roundEndMessageDiv.innerText = winMessage // Display "Player wins the round! You don't suck!"
+                playerScore = playerScore + 1 //Add to player score
+            }
         }
-    }
-    if(playerChoice === 'paper') {
-        if(computerChoice === 'scissors') {
-            // Display "Computer wins the round! You suck!"
-            computerScore = computerScore + 1 // Add to computer score
-        }
-        if(computerChoice === 'rock') {
-            // Display "Player wins the round! You don't suck!"
-            playerScore = playerScore + 1 //Add to player score
-        }
-    }
-    if(playerChoice === 'scissors') {
-        if(computerChoice === 'rock') {
-            // Display "Computer wins the round! You suck!"
-            computerScore = computerScore + 1 // Add to computer score
-        }
-        if(computerChoice === 'paper') {
-            // Display "Player wins the round! You don't suck!"
-            playerScore = playerScore + 1 //Add to player score
+        if (playerChoice === 'scissors') {
+            if (computerChoice === 'rock') {
+                roundEndMessageDiv.innerText = loseMessage // Display "Computer wins the round! You suck!"
+                computerScore = computerScore + 1 // Add to computer score
+            }
+            if (computerChoice === 'paper') {
+                roundEndMessageDiv.innerText = winMessage // Display "Player wins the round! You don't suck!"
+                playerScore = playerScore + 1 //Add to player score
+            }
         }
     }
 
     // Display Scores
+    document.getElementById('player-score').innerText = playerScore
+    document.getElementById('computer-score').innerText = computerScore
 
-    if(playerScore + computerScore === 5) {
+    if (playerScore + computerScore === 5) {
         endGame()
     }
 }
 
 function endGame() {
-    if(playerScore > computerScore) {
+    document.getElementById('choices').style.display = 'none' // Hide buttons so can't continue
+    document.getElementById('end-screen').style.display = 'inline' // Hide buttons so can't continue
+    let randomNumber = Math.floor(Math.random() * computerArray.length)
+    if (playerScore > computerScore) {
         // Display "You won the game!"
         // Add a 'random' compliment from an array as well
     } else {
         // Display "You lost the game!"
         // Add a 'random' insult from an array as well
     }
-    // Hide buttons so can't continue
-    // Pop up the reset game button
 }
 
 function resetGame() {
     // Remove the stuff added by endGame()
-    // Add the buttons back in
+    document.getElementById('choices').style.display = 'inline' // Add the buttons back in
 }
