dd564961787f7767e8339af385f2c271a92444a7
J-Groesbeck <jacob.groesbeck@icloud.com>
2024-02-15 14:41:47 -0500

    Added options and also most of functionality is done

--- script.js
+++ script.js
@@ -2,22 +2,26 @@ let userName = ''
 
 function inputName() {
     userName = prompt('Enter your name:')
-    document.getElementById('input-name').style.display = "none" // Hide the input name button
-    document.getElementById('game').style.display = "inline" // Show the game
-    document.querySelectorAll('.name').forEach(element => element.innerText = userName) // Display the name in all appropriate places
+    if (userName === null || !userName.trim()) {
+        alert('Please enter a valid name!')
+    } else {
+        document.getElementById('input-name').style.display = "none" // Hide the input name button
+        document.getElementById('game').style.display = "inline" // Show the game
+        document.querySelectorAll('.name').forEach(element => element.innerText = userName) // Display the name in all appropriate places
+    }
 }
 
 let playerScore = 0
 let computerScore = 0
 
 function oneRound(playerChoice) {
-    let computerArray = ['rock', 'paper', 'scissors']
+    let computerArray = ['Rock', 'Paper', 'Scissors']
     let randomNumber = Math.floor(Math.random() * computerArray.length)
     let computerChoice = computerArray[randomNumber]
 
     // Display choices
     document.getElementById('player-choice').innerText = playerChoice
-    document.getElementById('player-choice').innerText = computerChoice
+    document.getElementById('computer-choice').innerText = computerChoice
 
     // Variables for declaring results
     let winMessage = `${userName} wins the round! You don't suck!`
@@ -27,32 +31,32 @@ function oneRound(playerChoice) {
     if (computerChoice === playerChoice) {
         roundEndMessageDiv.innerText = "It's a draw!" //Display "It's a draw!"
     } else {
-        (playerChoice === 'rock') {
-            if (computerChoice === 'paper') {
+        if (playerChoice === 'Rock') {
+            if (computerChoice === 'Paper') {
                 roundEndMessageDiv.innerText = loseMessage // Display "Computer wins the round! You suck!"
                 computerScore = computerScore + 1 // Add to computer score
             }
-            if (computerChoice === 'scissors') {
+            if (computerChoice === 'Scissors') {
                 roundEndMessageDiv.innerText = winMessage // Display "Player wins the round! You don't suck!"
                 playerScore = playerScore + 1 //Add to player score
             }
         }
-        if (playerChoice === 'paper') {
-            if (computerChoice === 'scissors') {
+        if (playerChoice === 'Paper') {
+            if (computerChoice === 'Scissors') {
                 roundEndMessageDiv.innerText = loseMessage // Display "Computer wins the round! You suck!"
                 computerScore = computerScore + 1 // Add to computer score
             }
-            if (computerChoice === 'rock') {
+            if (computerChoice === 'Rock') {
                 roundEndMessageDiv.innerText = winMessage // Display "Player wins the round! You don't suck!"
                 playerScore = playerScore + 1 //Add to player score
             }
         }
-        if (playerChoice === 'scissors') {
-            if (computerChoice === 'rock') {
+        if (playerChoice === 'Scissors') {
+            if (computerChoice === 'Rock') {
                 roundEndMessageDiv.innerText = loseMessage // Display "Computer wins the round! You suck!"
                 computerScore = computerScore + 1 // Add to computer score
             }
-            if (computerChoice === 'paper') {
+            if (computerChoice === 'Paper') {
                 roundEndMessageDiv.innerText = winMessage // Display "Player wins the round! You don't suck!"
                 playerScore = playerScore + 1 //Add to player score
             }
@@ -71,7 +75,9 @@ function oneRound(playerChoice) {
 function endGame() {
     document.getElementById('choices').style.display = 'none' // Hide buttons so can't continue
     document.getElementById('end-screen').style.display = 'inline' // Hide buttons so can't continue
-    let randomNumber = Math.floor(Math.random() * computerArray.length)
+    let complimentArray = ['', '', '']
+    let insultArray = ['', '', '']
+    let randomNumber = Math.floor(Math.random() * complimentArray.length)
     if (playerScore > computerScore) {
         // Display "You won the game!"
         // Add a 'random' compliment from an array as well
